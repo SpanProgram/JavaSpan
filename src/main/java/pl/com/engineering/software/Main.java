43f56cf15ecb56bdd345956ccfd638722a171e82
Rafal Dworznik <rapiernik@gmail.com>
2018-09-20 22:19:00 +0200

    First working Java-DXF integration

--- src/main/java/pl/com/engineering/software/Main.java
+++ src/main/java/pl/com/engineering/software/Main.java
@@ -1,8 +1,55 @@
 package pl.com.engineering.software;
 
+import com.jsevy.jdxf.DXFDocument;
+import com.jsevy.jdxf.DXFGraphics;
+
+import java.awt.*;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Span");
+        DXFDocument dxfDocument = new
+                DXFDocument("First dxf output");
+        DXFGraphics dxfGraphics =
+                dxfDocument.getGraphics();
+
+        paint(dxfGraphics);
+
+        String stringOutput = dxfDocument.toDXFString();
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter("E:/dxfOutput.dxf");
+            fileWriter.write(stringOutput);
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void paint(DXFGraphics graphics) {
+        graphics.setColor(Color.RED);
+        graphics.setStroke(new BasicStroke(3));
+
+        graphics.drawLine(0, 0, 1000, 500);
+        graphics.drawRect(1000, 500, 150, 150);
+        graphics.drawRoundRect(20, 200, 130, 100, 20,
+                10);
+        graphics.drawOval(200, 800, 200, 400);
+        graphics.drawArc(100, 1900, 400, 200, 60, 150);
+
+        graphics.setColor(Color.BLUE);
+        graphics.fillRect(100, 100, 100, 50);
+        int[] xPoints = {200, 300, 250};
+        int[] yPoints = {200, 250, 300};
+        graphics.fillPolygon(xPoints, yPoints, xPoints.length);
+
+        graphics.setFont(new Font("TimesRoman",Font.BOLD, 18));
+        graphics.drawString("Some 38-point monospaced blue text at position 480, 400", 480, 400);
+
+        graphics.shear(0.1f, 0.2f);
+        graphics.drawRect(100, 100, 200, 200);
     }
 }
